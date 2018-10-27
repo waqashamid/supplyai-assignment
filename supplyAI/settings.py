1bc09b0d1cfa9a490897036d55b9c7bc8764c6d9
Waqas Hamid <waqashamid722@gmail.com>
2018-10-27 17:11:04 +0530

    Fixing static dirs

--- supplyAI/settings.py
+++ supplyAI/settings.py
@@ -136,9 +136,9 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 STATIC_TMP = os.path.join(BASE_DIR, 'static')
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'supplyAI/static'),
-)
+# # Extra places for collectstatic to find static files.
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, '/static'),
+# )
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
