deac729503f853b7308dddfd1bd9377993a60847
Waqas Hamid <waqashamid722@gmail.com>
2018-10-27 16:51:18 +0530

    Running collectstatic

--- supplyAI/settings.py
+++ supplyAI/settings.py
@@ -134,6 +134,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+STATIC_TMP = os.path.join(BASE_DIR, 'static')
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
