ad1f2203342733b90be15e734dcc2199343a6b0e
Waqas Hamid <waqashamid722@gmail.com>
2018-10-28 09:31:53 +0530

    Added csrf exemption

--- bouncer/urls.py
+++ bouncer/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import url
 from .api import *
-from django.views.generic import TemplateView
+from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     url(r'^user/register$', RegisterUser.as_view(), name='register_user'),
-    url(r'^user/signup$', FetchRegistrationForm.as_view(), name='signup_form'),
+    url(r'^user/signup$', csrf_exempt(FetchRegistrationForm.as_view()), name='signup_form'),
     url(r'^user/email/verify/(?P<user_id>[\w.-]+)/(?P<key>[\w.-]+)$', VerifyEmailRegisterUser.as_view(),
         name='verify_email'),
     url(r'^products/(?P<start>[\w.-]+)/(?P<stop>[\w.-]+)$', FetchProducts.as_view(), name='products'),
