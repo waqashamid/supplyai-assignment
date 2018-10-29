a0930c6e2487d7fa3dea44ad31a000979dec5590
Waqas Hamid <waqashamid722@gmail.com>
2018-10-29 13:09:31 +0530

    Changed email password

--- supplyAI/settings.py
+++ supplyAI/settings.py
@@ -82,7 +82,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'waqas7hamid@gmail.com'
-EMAIL_HOST_PASSWORD = 'testemail123'
+EMAIL_HOST_PASSWORD = 'testemail123!'
 EMAIL_PORT = 587
 
 # # Database
